877fd5f79f0cce2fa3d60d6d31376b2d6262d5ae
Rodrigo Azevedo <zvdrodrigo@gmail.com>
2025-05-12 15:34:13 -0300

    test jenkinsfile

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -16,7 +16,7 @@ terraform {
     region = "placeholder"
 
     endpoints = {
-      s3 = "http://172.19.0.5:9000"
+      s3 = "http://localhost:9000"
     }
 
     skip_credentials_validation = true
