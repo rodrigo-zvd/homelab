a6da388c9d39784d65c88178cf79f6043d0b2ceb
Rodrigo Azevedo <zvdrodrigo@gmail.com>
2025-05-16 19:35:33 -0300

    test

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -35,7 +35,7 @@ terraform {
 provider "xenorchestra" {
   # Must be ws or wss
   url      = "ws://xen-orchestra:80" # Or set XOA_URL environment variable
-  # username = "admin@admin.net"                   # Or set XOA_USER environment variable
+  # username = "admin@admin.net"     # Or set XOA_USER environment variable
   # password = "admin"               # Or set XOA_PASSWORD environment variable
 
   # This is false by default and
